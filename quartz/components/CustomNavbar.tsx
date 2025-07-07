98ebdea27ffd7d9fa5c777aba7ab83683634c3af
bendwild <dewildebenjamin0@gmail.com>
2025-07-07 12:28:09 +0200

    Quartz sync: Jul 7, 2025, 12:28 PM

---
+++ quartz/components/CustomNavbar.tsx
@@ -0,0 +1,29 @@
+import { QuartzComponent, QuartzComponentConstructor } from "../types"
+import "./CustomNavbar.css"
+
+const CustomNavbar: QuartzComponent = () => {
+  return (
+    <nav className="main-navigation">
+      <div className="nav-container">
+        <div className="nav-logo">
+          <a href="/">
+            <img
+              src="https://raw.githubusercontent.com/bendwild/afromedica/v4/content/Extra/Images/afromedica%20(6).png"
+              alt="AfroMedica Logo"
+            />
+          </a>
+        </div>
+        <ul className="nav-menu">
+          <li><a href="/" className="nav-link">Home</a></li>
+          <li><a href="/about" className="nav-link">About Us</a></li>
+          <li><a href="/afrocademy" className="nav-link">Afrocademy</a></li>
+          <li><a href="/projects" className="nav-link">Projects</a></li>
+          <li><a href="/team" className="nav-link">Team</a></li>
+          <li><a href="/contact" className="nav-link">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default (() => CustomNavbar) satisfies QuartzComponentConstructor
