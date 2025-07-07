035f9692a19876c4c9aaf7128967c092d739a68a
bendwild <dewildebenjamin0@gmail.com>
2025-07-07 13:53:45 +0200

    Update CustomNavbar.tsx

--- quartz/components/CustomNavbar.tsx
+++ quartz/components/CustomNavbar.tsx
@@ -1,11 +1,11 @@
-import { QuartzComponent, QuartzComponentConstructor } from "../types"
+const basePath = "/afro"
 
 const CustomNavbar: QuartzComponent = () => {
   return (
     <nav className="main-navigation">
       <div className="nav-container">
         <div className="nav-logo">
-          <a href="/">  {/* <-- use / instead of /index */}
+          <a href={`${basePath}/`}>
             <img
               src="https://raw.githubusercontent.com/bendwild/afro/v4/content/Extra/Images/afromedica%20(6).png"
               alt="AfroMedica Logo"
@@ -13,15 +13,13 @@ const CustomNavbar: QuartzComponent = () => {
           </a>
         </div>
         <ul className="nav-menu">
-          <li><a href="/about" className="nav-link">About Us</a></li>
-          <li><a href="/afrocademy" className="nav-link">Afrocademy</a></li>
-          <li><a href="/projects" className="nav-link">Projects</a></li>
-          <li><a href="/team" className="nav-link">Team</a></li>
-          <li><a href="/contact" className="nav-link">Contact</a></li>
+          <li><a href={`${basePath}/about`} className="nav-link">About Us</a></li>
+          <li><a href={`${basePath}/afrocademy`} className="nav-link">Afrocademy</a></li>
+          <li><a href={`${basePath}/projects`} className="nav-link">Projects</a></li>
+          <li><a href={`${basePath}/team`} className="nav-link">Team</a></li>
+          <li><a href={`${basePath}/contact`} className="nav-link">Contact</a></li>
         </ul>
       </div>
     </nav>
   )
 }
-
-export default (() => CustomNavbar) satisfies QuartzComponentConstructor
